86a57048ecc5231f7e6184ecc3a9f13098819e5a
kingsman20 <kingslifeudo@gmail.com>
2020-10-27 12:21:33 +0100

    Fixed env path

--- src/utils/createJWT.ts
+++ src/utils/createJWT.ts
@@ -1,13 +1,11 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const createJWT = (id: string): string => {
   const secret = process.env.JWT_SECRET || '';
   const token = jwt.sign({ id }, secret, { expiresIn: '1d' });
   return token;
 };
-
-
 
 export default createJWT;
