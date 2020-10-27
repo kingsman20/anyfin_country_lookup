4053be52e390829628bdc33735e7a93d9ce9c42e
kingsman20 <kingslifeudo@gmail.com>
2020-10-27 12:19:20 +0100

    Generated types

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,9 +1,8 @@
-export const typeDefs = ["type Query {\n  countries(name: String): [Country!]!\n  sayHello: String!\n}\n\ntype Country {\n  name: String!\n  population: Int!\n  currencies: [Currency!]!\n}\n\ntype Currency {\n  base: String!\n  date: String!\n  rates: [Rate!]!\n}\n\ntype Rate {\n  SEK: String!\n}\n\ntype User {\n  id: ID!\n  email: String!\n  token: String!\n}\n\ntype Mutation {\n  LoginUser(email: String!, password: String!): User!\n}\n"];
+export const typeDefs = ["type Query {\n  countries(name: String): [Country!]!\n}\n\ntype Country {\n  name: String!\n  population: Int!\n  currencies: [Currency!]!\n}\n\ntype Currency {\n  base: String!\n  date: String!\n  rates: [Rate!]!\n}\n\ntype Rate {\n  SEK: String!\n}\n\ntype User {\n  id: ID!\n  email: String!\n  token: String!\n}\n\ntype Mutation {\n  LoginUser(email: String!, password: String!): User!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   countries: Array<Country>;
-  sayHello: string;
 }
 
 export interface CountriesQueryArgs {
