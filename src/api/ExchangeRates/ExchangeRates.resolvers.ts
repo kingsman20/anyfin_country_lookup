420a395f991b2b2172d60ee1d227ff024cfcadfb
kingsman20 <kingslifeudo@gmail.com>
2020-10-29 09:42:10 +0100

    Added exchange rates endpoint

--- src/api/ExchangeRates/ExchangeRates.resolvers.ts
+++ src/api/ExchangeRates/ExchangeRates.resolvers.ts
@@ -1,8 +1,14 @@
 import fetch from 'node-fetch';
-import { CountriesQueryArgs, Country } from 'src/types/graph';
+import {
+  CountriesQueryArgs,
+  Country,
+  CurrenciesQueryArgs,
+} from 'src/types/graph';
 import privateResolver from '../../utils/privateResolver';
 
 const countryBaseURL = 'https://restcountries.eu/rest/v2';
+const exchangeRateBaseURL = 'http://data.fixer.io/api';
+// http://data.fixer.io/api/latest?access_key=de6f611b0b1cf6021e1647a473b9cfd1&base=EUR&symbols=SEK
 
 const resolvers = {
   Query: {
@@ -14,6 +20,20 @@ const resolvers = {
         return responseData;
       }
     ),
+
+    currencies: privateResolver(async (_, args: CurrenciesQueryArgs) => {
+      const symbols = args;
+      console.log(symbols);
+      
+      const response = await fetch(
+        `${exchangeRateBaseURL}/latest?access_key=${process.env.ACCESS_KEY}&base=EUR&symbols=${symbols.symbols}`
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      
+      
+      return responseData;
+    }),
   },
 };
 
--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,12 +1,17 @@
-export const typeDefs = ["type Query {\n  countries(code: String): [Country!]!\n}\n\ntype Country {\n  name: String!\n  population: Int!\n  flag: String!\n  alpha2Code: String!\n}\n\ntype Currency {\n  base: String!\n  date: String!\n  rates: [Rate!]!\n}\n\ntype Rate {\n  SEK: String!\n}\n\ntype User {\n  id: ID!\n  email: String!\n  token: String!\n}\n\ntype Mutation {\n  LoginUser(email: String!, password: String!): User!\n}\n"];
+export const typeDefs = ["type Query {\n  countries(code: String): [Country!]!\n  currencies(symbols: String): Currency!\n}\n\ntype Country {\n  name: String!\n  population: Int!\n  flag: String!\n  alpha2Code: String!\n}\n\ntype Currency {\n  base: String!\n  rates: Rate!\n}\n\ntype Rate {\n  SEK: String\n}\n\ntype User {\n  id: ID!\n  email: String!\n  token: String!\n}\n\ntype Mutation {\n  LoginUser(email: String!, password: String!): User!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   countries: Array<Country>;
+  currencies: Currency;
 }
 
 export interface CountriesQueryArgs {
   code: string | null;
+}
+
+export interface CurrenciesQueryArgs {
+  symbols: string | null;
 }
 
 export interface Country {
@@ -14,6 +19,15 @@ export interface Country {
   population: number;
   flag: string;
   alpha2Code: string;
+}
+
+export interface Currency {
+  base: string;
+  rates: Rate;
+}
+
+export interface Rate {
+  SEK: string | null;
 }
 
 export interface Mutation {
@@ -29,14 +43,4 @@ export interface User {
   id: string;
   email: string;
   token: string;
-}
-
-export interface Currency {
-  base: string;
-  date: string;
-  rates: Array<Rate>;
-}
-
-export interface Rate {
-  SEK: string;
 }
